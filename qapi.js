0bd08a5afc804253f2bfd37790996418b5a320e0
Rim <61412363+Juanr1m@users.noreply.github.com>
2020-12-15 12:38:47 +0500

    Changes

---
+++ qapi.js
@@ -0,0 +1,31 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa7e584b83a2f001f2d7a61",
+				"5fa7e584b83a2f001f2d7a63",
+				"5fa7edbeb83a2f001f2d7b14"
+			]
+		},
+		"5fa7e584b83a2f001f2d7a61": {
+			"id": "5fa7e584b83a2f001f2d7a61",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa7e584b83a2f001f2d7a63": {
+			"id": "5fa7e584b83a2f001f2d7a63",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5fa7edbeb83a2f001f2d7b14": {
+			"id": "5fa7edbeb83a2f001f2d7b14",
+			"pageUrl": "linked",
+			"name": "linked"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
